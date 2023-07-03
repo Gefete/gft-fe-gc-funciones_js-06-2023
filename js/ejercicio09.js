96219e408222ab9d90aa8eafafad569ec1e0aba8
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-03 11:30:18 +0200

    09

--- js/ejercicio09.js
+++ js/ejercicio09.js
@@ -3,3 +3,30 @@ rellenaremos el array con números aleatorios entre 0 y 9. Al final muestra por 
 valor de cada posición y la suma de todos los valores. Tareas a realizar: Haz un método 
 para rellenar el array(que tenga como parámetros los números entre los que tenga que 
 generar) y otro para mostrar el contenido y la suma del array */
+
+
+
+const generateRandom = (size) =>{
+    let array = [];
+    if(!Number.isInteger(size)){
+        return null;
+    }
+    for (let i = 0; i < size; i++) {
+        let randomnum = Math.floor(Math.random() * 9);;
+        array.push(randomnum);
+        console.log("posicion "+i+" tenemos e valor: "+randomnum);
+    }
+    return array;
+}
+
+const sumaArray = (array) => {
+    let suma = 0;
+    for (let i = 0; i < array.length; i++) {
+        suma += array[i]
+    }
+    console.log("La suma de todos los numeross da un valor de "+suma);
+}
+
+let sizeArray = +prompt("Indica el tamaño de la array que quieres generar");
+let arrayrandoms = generateRandom(sizeArray);
+sumaArray(arrayrandoms);
